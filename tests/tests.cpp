ef857d7bb584910c9bdec7bf306e0e985081f2f2
Angela Zhao <alz3@linux-a2.ews.illinois.edu>
2020-11-30 21:06:18 -0600

    update DEVELOPMENT.md and add test cases

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,20 +2,30 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <algorithm>
 
 #include "../graph.h"
 // Tests not working??? No vertices and edges are getting added??? Code seems to work fine in main
 // Issue is probably with file reading???
 TEST_CASE("Checking invalid argument detection in vertex constructor", "[vertex][graph]") {
-  Graph g("invalid-airports.dat", "sample_routes.dat");
+  Graph g("tests/invalid-airports.dat", "tests/sample_routes.dat");
   // Check that no vertices or edges are added to the graph when provided with invalid inputs
   REQUIRE(g.GetNumVertices() == 0);
   REQUIRE(g.GetNumEdges() == 0);
 }
 
 TEST_CASE("Parsing a small graph", "[graph]") {
-  Graph g("sample_airports.dat", "sample_routes.dat");
+  Graph g("tests/sample_airports.dat", "tests/sample_routes.dat");
   REQUIRE(g.GetNumVertices() == 10);
   REQUIRE(g.GetNumEdges() == 20);
   REQUIRE(g.VertexExists("ORD"));
+  REQUIRE(g.EdgeExists("ORD", "LAX"));
+  REQUIRE(g.EdgeExists("LAX", "ORD"));
+  REQUIRE(!g.VertexExists("LGA"));
+  REQUIRE(!g.EdgeExists("ORD", "CLT"));
+  std::vector<std::string> neighbors = g.GetDestinations("ORD");
+  std::sort(neighbors.begin(), neighbors.end());
+  REQUIRE(neighbors.size() == 2);
+  REQUIRE(neighbors[0] == "DFW");
+  REQUIRE(neighbors[1] == "LAX");
 }
