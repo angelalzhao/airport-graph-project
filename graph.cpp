09db42ece6bff37774ff4e0df9a43942fcdf9ccd
Angela Zhao <alz3@linux-a2.ews.illinois.edu>
2020-12-10 05:00:33 -0600

    pagerank update 2

--- graph.cpp
+++ graph.cpp
@@ -259,25 +259,24 @@ std::pair<std::unordered_map<std::string, double>, std::vector<std::pair<double,
     ranking.insert(std::make_pair(v.first, 1.0 / vertices.size()));
   }
   while (!converged) {
-    //int no_outgoing = 0;
+    double no_outgoing = 0;
     previous = ranking;
     for (auto & r : ranking) {
       r.second = 0;
     }
     for (const auto & r : ranking) {
-      // TODO: handle no outgoing edge case
-      /*if (!adj_list.count(r.first)) {
-        no_outgoing++;
+      if (!adj_list.count(r.first)) {
+        no_outgoing += previous[r.first];
         continue;
-      }*/
+      }
       for (const std::string & v : adj_list[r.first]) {
         int connections = adj_list[r.first].size();
-        ranking[v] += decay * previous[r.first] / (connections == 0 ? vertices.size() : connections);
+        ranking[v] += decay * previous[r.first] / connections;
       }
     }
     // for (const auto & elem : ranking) std::cout << elem.first << " - " << elem.second << " " << std::endl;
     for (auto & r : ranking) {
-      //r.second += decay * (double) no_outgoing * previous[r.first] / vertices.size();
+      r.second += decay * no_outgoing * previous[r.first] / vertices.size();
       r.second += (1.0 - decay) / vertices.size();
     }
     // for (const auto & elem : ranking) std::cout << elem.first << " - " << elem.second << " " << std::endl;
